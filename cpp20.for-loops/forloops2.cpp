91f16aec0c746d4b7331cf043f3e98c3bd5b0e84
marki1n-code <mramur47@gmail.com>
2026-01-24 17:38:53 +0200

    Refactor code structure for improved readability and maintainability

--- cpp20.for-loops/forloops2.cpp
+++ cpp20.for-loops/forloops2.cpp
@@ -4,7 +4,7 @@ int main () {
 
     // i = index
 
-    for (int i = 1; i <= 7; i++)  // reapets code 3 times
+    for (int i = 1; i <= 7; i++)  // reapets code 7 times  1,2,3...7
     {
         std::cout << i << std::endl; 
 
---
+++ cpp20.for-loops/forloops3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main () {
+
+    // i = index
+
+    for (int i = 10; i >- 0; i--)  // reapets code x times 10,9,8...1
+    {
+        std::cout << i << std::endl; 
+
+        /* code */
+    }
+    
+    std::cout << "Heppy new year! ";
+
+    return 0;
+}
